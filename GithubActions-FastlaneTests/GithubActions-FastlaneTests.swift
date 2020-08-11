bfd6084e0679c1ec4a2041d16fe615e3e27b5e78
Vitor Demenighi <vitor_m2@hotmail.com>
2020-08-11 16:16:03 -0300

    swift lint

--- GithubActions-FastlaneTests/GithubActions-FastlaneTests.swift
+++ GithubActions-FastlaneTests/GithubActions-FastlaneTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import GithubActions_Fastlane
 
-class GithubActions_FastlaneTests: XCTestCase {
+class GithubActionsFastlaneTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
