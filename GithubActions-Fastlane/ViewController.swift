72e1a12a135ffd600083cfc2104525ef69bca47a
Vitor Demenighi <vitor_m2@hotmail.com>
2020-08-11 14:24:12 -0300

    create project

---
+++ GithubActions-Fastlane/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubActions-Fastlane
+//
+//  Created by Vitor Demenighi on 11/08/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
